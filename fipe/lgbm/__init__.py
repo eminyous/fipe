0878af06aed3cb661d0297a7d4e23bb71337546c
eminyous <youssouf.emine@polymtl.ca>
2024-11-06 19:52:03 -0500

    export Pruner in lgbm module.

--- fipe/lgbm/__init__.py
+++ fipe/lgbm/__init__.py
@@ -1,5 +1,6 @@
 from .ensemble import Ensemble
 from .fipe import FIPE
 from .oracle import Oracle
+from .prune import Pruner
 
-__all__ = ["FIPE", "Ensemble", "Oracle"]
+__all__ = ["FIPE", "Ensemble", "Oracle", "Pruner"]
