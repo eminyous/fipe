e930fd15f130f69f7712e37a77c98bc23f6d9184
eminyous <youssouf.emine@polymtl.ca>
2025-01-05 02:25:36 -0500

    Remove TreeParser and create_parser from __all__ in tree module

--- fipe/tree/__init__.py
+++ fipe/tree/__init__.py
@@ -1,10 +1,7 @@
 from .container import TreeContainer
-from .parsers import TreeParser, create_parser
 from .tree import Tree
 
 __all__ = [
     "Tree",
     "TreeContainer",
-    "TreeParser",
-    "create_parser",
 ]
